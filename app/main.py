c9ef0c9cadf4b09731e7675482d752b2443abf85
lkarjun <lk.arjun@hotmail.com>
2022-05-17 13:58:59 +0530

    fixes lint issues

--- app/main.py
+++ app/main.py
@@ -10,10 +10,3 @@ def read_root():
     Return Hello Github CI/CD
     """
     return {"Hello": "Github CI/CD"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    """
-    Return item_id with random number
-    """
-    return {"item_id": item_id, "q": random()}
