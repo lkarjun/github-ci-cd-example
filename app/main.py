652480e26ad9dcc4cccb9a70dc9094b8bcf36db9
lkarjun <lk.arjun@hotmail.com>
2022-05-17 13:56:57 +0530

    fixes lint issues

--- app/main.py
+++ app/main.py
@@ -6,11 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    """return Hello Github CI/CD"""
+    """
+    Return Hello Github CI/CD
+    """
     return {"Hello": "Github CI/CD"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    """return item_id with random number"""
-    return {"item_id": item_id, "q": random()}
